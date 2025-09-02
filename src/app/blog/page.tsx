09ab501430ad0509414a0eec0d96763822deb7a2
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-09-02 23:12:17 +0200

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,7 @@ import path from 'path';
 import ReactMarkdown from 'react-markdown';
 
 export default async function BlogPage() {
-  const filePath = path.join(process.cwd(), 'post1.md');
+  const filePath = path.join(process.cwd(), 'post1.md'); // path to your markdown
   const content = fs.readFileSync(filePath, 'utf8');
 
   return (
